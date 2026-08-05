e9a769416d6807cab76c3463548e12f248db3e21
xkyrell <xkyrell@gmail.com>
2026-08-05 11:01:53 +0300

    feat(core): add temporal value utility methods

--- temporal-core/src/main/java/me/xkyrell/temporal/TemporalValue.java
+++ temporal-core/src/main/java/me/xkyrell/temporal/TemporalValue.java
@@ -14,6 +14,17 @@ public interface TemporalValue extends Comparable<TemporalValue> {
     long get(@NotNull TemporalUnit unit);
 
     /**
+     * Returns whether this value is greater than or equal to the specified value.
+     *
+     * @param value the value to compare to
+     * @return {@code true} if this value is greater than or equal to the specified value, otherwise {@code false}
+     * @since 2.0
+     */
+    default boolean isGreaterThanOrEqual(@NotNull TemporalValue value) {
+        return compareTo(value) >= 0;
+    }
+
+    /**
      * Returns whether this value is greater than the specified value.
      *
      * @param value the value to compare to
@@ -25,6 +36,17 @@ public interface TemporalValue extends Comparable<TemporalValue> {
     }
 
     /**
+     * Returns whether this value is less than or equal to the specified value.
+     *
+     * @param value the value to compare to
+     * @return {@code true} if this value is less than or equal to the specified value, otherwise {@code false}
+     * @since 2.0
+     */
+    default boolean isLessThanOrEqual(@NotNull TemporalValue value) {
+        return compareTo(value) <= 0;
+    }
+
+    /**
      * Returns whether this value is less than the specified value.
      *
      * @param value the value to compare to
@@ -33,5 +55,35 @@ public interface TemporalValue extends Comparable<TemporalValue> {
      */
     default boolean isLessThan(@NotNull TemporalValue value) {
         return compareTo(value) < 0;
+    }
+
+    /**
+     * Returns whether this value is positive.
+     *
+     * @return {@code true} if this value is positive, otherwise {@code false}
+     * @since 2.0
+     */
+    default boolean isPositive() {
+        return get(TemporalUnit.MILLIS) > 0L;
+    }
+
+    /**
+     * Returns whether this value is negative.
+     *
+     * @return {@code true} if this value is negative, otherwise {@code false}
+     * @since 2.0
+     */
+    default boolean isNegative() {
+        return get(TemporalUnit.MILLIS) < 0L;
+    }
+
+    /**
+     * Returns whether this value is zero.
+     *
+     * @return {@code true} if this value is zero, otherwise {@code false}
+     * @since 1.0
+     */
+    default boolean isZero() {
+        return get(TemporalUnit.MILLIS) == 0L;
     }
 }
