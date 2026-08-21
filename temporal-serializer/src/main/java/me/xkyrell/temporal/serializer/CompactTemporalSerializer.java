2f51b13cbf910fb60fbd065dbe17bf7c55a8cfc0
xkyrell <xkyrell@gmail.com>
2026-08-21 19:29:19 +0300

    feat(serializer): add CompactTemporalSerializer

---
+++ temporal-serializer/src/main/java/me/xkyrell/temporal/serializer/CompactTemporalSerializer.java
@@ -0,0 +1,37 @@
+package me.xkyrell.temporal.serializer;
+
+import me.xkyrell.temporal.Temporal;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import java.util.function.Supplier;
+
+/**
+ * A serializer for compact temporal values such as {@code 01:20:30}.
+ *
+ * @see CompactTemporalFormat
+ * @see TemporalSerializer
+ * @since 2.0
+ */
+public interface CompactTemporalSerializer
+        extends TemporalSerializer<Temporal, CompactTemporalFormat, String> {
+
+    @Override
+    default Temporal deserialize(@NotNull String input) {
+        return deserialize(input, CompactTemporalFormat.defaults());
+    }
+
+    @Override
+    default Temporal deserializeOrFallback(@NotNull String input, @NotNull Supplier<Temporal> onFallback) {
+        return deserializeOrFallback(input, CompactTemporalFormat.defaults(), onFallback);
+    }
+
+    @Override
+    default String serialize(@NotNull Temporal temporal) {
+        return serialize(temporal, CompactTemporalFormat.defaults());
+    }
+
+    @Override
+    default String serializeOrFallback(@NotNull Temporal temporal, @NotNull Supplier<String> onFallback) {
+        return serializeOrFallback(temporal, CompactTemporalFormat.defaults(), onFallback);
+    }
+}
